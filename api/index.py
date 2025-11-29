6d1a9dae60f5e1fc4d4f841d6da3a45e28365fa3
Resml <tanajijadhav166@gmail.com>
2025-11-29 12:07:57 +0530

    working 2

---
+++ api/index.py
@@ -0,0 +1,7 @@
+import os
+import sys
+
+# Add the project root to the python path so we can import from backend
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from backend.main import app
